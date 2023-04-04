f6bca6b34286dc28b86bf7f34829743293b19c73
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-04-04 19:43:04 -0300

    components

--- src/styles.js
+++ src/styles.js
@@ -9,4 +9,18 @@ import styled from "styled-components"
 
     // 3 - crie um componente com a sintaxe abaixo
     // 4 - utilize o componente criado no arquivo que desejar
+    // 5 - para estilizar o componente, utilize template literals (``) e dentro delas, utilize a sintaxe css
+    // 6 - para utilizar o componente em mais de um arquivo, exporte-o
+
+
+// Exemplo de componente criado com styled-components 
+// Pra a estilização do componente, utilize template literals (``) e dentro delas, utilize a sintaxe css   
+const Titulo = styled.h1`
+    color: #f00;
+    font-size: 20px;
+
+`;
     
+const Botao = styled.button`
+    background: #f00;
+`;
